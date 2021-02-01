a2f6e4cbc995dc5ced6580c686ba5633a0e92f74
fahimchowdhury43 <fahimc43@gmail.com>
2021-02-02 04:11:25 +0600

    null vs undefined for clear question

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+// let name;
+// console.log(name);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(13, 43);
+// console.log(result);
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const lover = {name:"smart dude", phone:"17345"};
+// console.log(lover.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [12, 23, 32]
+console.log(ages[11]);
