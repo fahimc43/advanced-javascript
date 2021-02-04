853f43928ed5d9c2a1dac505e44b0f90a3d0785e
fahimchowdhury43 <fahimc43@gmail.com>
2021-02-05 02:02:08 +0600

    bind to borrow method from another object

--- callApply.js
+++ callApply.js
@@ -1,3 +1,25 @@
+// const normalParson = {
+//     firstName: "Rahim",
+//     lastName: "Uddin",
+//     salary: 15000,
+//     getFullName: function(){
+//         console.log(this.firstName, this.lastName);
+//     },
+//     chargeBill: function(amount){
+//         this.salary = this.salary - amount;
+//         return this.salary;
+//     }
+// }
+// // console.log(normalParson.firstName);
+// // normalParson.chargeBill(150);
+// // normalParson.chargeBill(3000);
+// // console.log(normalParson.salary);
+
+
+
+
+
+
 const normalParson = {
     firstName: "Rahim",
     lastName: "Uddin",
@@ -6,11 +28,33 @@ const normalParson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
-console.log(normalParson.firstName);
-normalParson.chargeBill(150);
-normalParson.chargeBill(3000);
+
+const heroParson = {
+    firstName: "Hero",
+    lastName: "Balam",
+    salary: 25000
+}
+
+const friendlyParson = {
+    firstName: "Hero",
+    lastName: "Golam",
+    salary: 25000
+}
+
+// normalParson.chargeBill();
+
+const heroChargeBill = normalParson.chargeBill.bind(heroParson);
+heroChargeBill(2000);
+heroChargeBill(3000);
+console.log(heroParson.salary);
 console.log(normalParson.salary);
+
+
+const friendlyChargeBill = normalParson.chargeBill.bind(friendlyParson);
+friendlyChargeBill(1500);
+console.log(friendlyParson.salary);
