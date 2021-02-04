ef828bbcc96c7f3472fd6726607c372f8de699d8
fahimchowdhury43 <fahimc43@gmail.com>
2021-02-05 04:57:30 +0600

    difference between bind, call & apply

--- callApply.js
+++ callApply.js
@@ -1,3 +1,5 @@
+// Object method property >>>>
+
 // const normalParson = {
 //     firstName: "Rahim",
 //     lastName: "Uddin",
@@ -18,7 +20,50 @@
 
 
 
+// Use bind to borrow method >>>>
 
+// const normalParson = {
+//     firstName: "Rahim",
+//     lastName: "Uddin",
+//     salary: 15000,
+//     getFullName: function(){
+//         console.log(this.firstName, this.lastName);
+//     },
+//     chargeBill: function(amount){
+//         console.log(this);
+//         this.salary = this.salary - amount;
+//         return this.salary;
+//     }
+// }
+
+// const heroParson = {
+//     firstName: "Hero",
+//     lastName: "Balam",
+//     salary: 25000
+// }
+
+// const friendlyParson = {
+//     firstName: "Hero",
+//     lastName: "Golam",
+//     salary: 25000
+// }
+
+// // normalParson.chargeBill();
+
+// const heroChargeBill = normalParson.chargeBill.bind(heroParson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
+// console.log(heroParson.salary);
+// console.log(normalParson.salary);
+
+
+// const friendlyChargeBill = normalParson.chargeBill.bind(friendlyParson);
+// friendlyChargeBill(1500);
+// console.log(friendlyParson.salary);
+
+
+
+// difference between bind, call and apply >>>>
 
 const normalParson = {
     firstName: "Rahim",
@@ -27,9 +72,9 @@ const normalParson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -46,15 +91,9 @@ const friendlyParson = {
     salary: 25000
 }
 
-// normalParson.chargeBill();
+// normalParson.chargeBill.call(heroParson, 900, 100, 10);
+// normalParson.chargeBill.call(heroParson, 3000, 300, 30);
+// console.log(heroParson.salary);
 
-const heroChargeBill = normalParson.chargeBill.bind(heroParson);
-heroChargeBill(2000);
-heroChargeBill(3000);
+normalParson.chargeBill.apply(heroParson, [3000, 300, 30]);
 console.log(heroParson.salary);
-console.log(normalParson.salary);
-
-
-const friendlyChargeBill = normalParson.chargeBill.bind(friendlyParson);
-friendlyChargeBill(1500);
-console.log(friendlyParson.salary);
